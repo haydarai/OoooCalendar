4e295ae0102c7303f3150ddea75882e225fb756d
Haydar Ali Ismail <haydar_ai@yahoo.com>
2017-10-19 22:03:35 +0800

    Add test for special dates and times

--- src/test/java/com/haydar/oooocalendar/TestRocket.java
+++ src/test/java/com/haydar/oooocalendar/TestRocket.java
@@ -24,4 +24,95 @@ public class TestRocket {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Check whether both of these return the same kind of string
+     * Will fail on special occasions ("NOON", "MIDNIGHT", "FATHER'S DAY", etc)
+     */
+    @Test
+    public void testCurrentTimeStamp() {
+        try {
+            clock.setTimeZone(20); // Set it to your actual timezone
+            Assert.assertEquals(clock.getCurrentTimeStamp(), clock.getLocalCurrentTimeStamp());
+        } catch (NoDataException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Check whether it shows the right response on double tenth day
+     * Only succeed if your date is currently on double tenth day
+     */
+    @Test
+    public void testDoubleTenthDay() {
+        try {
+            clock.setTimeZone(20); // Set it to your actual timezone
+            Assert.assertTrue(clock.getLocalCurrentTimeStamp().contains("-10-10")
+                    && clock.getLocalCurrentTimeStamp().contains("DOUBLE-TENTH DAY"));
+        } catch (NoDataException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Check whether it shows the right response on father's day
+     * Only succeed if your date is currently on father's day
+     */
+    @Test
+    public void testFathersDay() {
+        try {
+            clock.setTimeZone(20); // Set it to your actual timezone
+            Assert.assertTrue(clock.getLocalCurrentTimeStamp().contains("-08-08")
+                    && clock.getLocalCurrentTimeStamp().contains("FATHER'S DAY"));
+        } catch (NoDataException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Check whether it shows the right response on father's day
+     * Only succeed if your date is currently on x'mas day
+     */
+    @Test
+    public void testXmasDay() {
+        try {
+            clock.setTimeZone(20); // Set it to your actual timezone
+            Assert.assertTrue(clock.getLocalCurrentTimeStamp().contains("-12-25")
+                    && clock.getLocalCurrentTimeStamp().contains("X'MAS DAY"));
+        } catch (NoDataException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Check whether it shows the right response on noon
+     * Only succeed if your time is currently at noon
+     */
+    @Test
+    public void testNoon() {
+        try {
+            clock.setTimeZone(20); // Set it to your actual timezone
+            Assert.assertTrue((clock.getLocalCurrentTimeStamp().contains("12:00:") ||
+                    clock.getLocalCurrentTimeStamp().contains("12:01:"))
+                    && clock.getLocalCurrentTimeStamp().contains("NOON"));
+        } catch (NoDataException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Check whether it shows the right response on midnight
+     * Only succeed if your time is currently at midnight
+     */
+    @Test
+    public void testMidnight() {
+        try {
+            clock.setTimeZone(20); // Set it to your actual timezone
+            Assert.assertTrue((clock.getLocalCurrentTimeStamp().contains("00:00:") ||
+                    clock.getLocalCurrentTimeStamp().contains("00:01:"))
+                    && clock.getLocalCurrentTimeStamp().contains("MIDNIGHT"));
+        } catch (NoDataException e) {
+            e.printStackTrace();
+        }
+    }
 }
